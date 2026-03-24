6c323875d7b6f97b4a0e3b1f41552e7392a7192b
nasimahmed <nasimxy444@gmail.com>
2026-03-25 01:49:08 +0600

    Add Alembic migrations for existing schema

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
 
 # The Engine: Connects to the DB hardware
-engine = create_engine("postgresql://user:password@db:5432/employee_db", pool_pre_ping=True)
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URI,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True
+)
 
 # The Factory: Creates a new 'db' instance for every request
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
